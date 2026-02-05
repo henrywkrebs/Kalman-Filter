6e4c3d2612a3c638372e55a368ce22a2b4cd277c
henrywkrebs <henrywkrebs@gmail.com>
2026-02-04 20:43:52 -0700

    getting started

---
+++ src/kalman_filter.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+sim_path = "vehicle_sim_data.csv"
+
+dt = 0.01
+
+Px = 0.00 #position on the x axis
+Py = 0.00 #position on the y axis
+Vx = 0.00 #velocity in the x axis
+Vy = 0.00 #velocity in the y axis
+
+
+matrix_np = ([[Px],
+              [Py],
+              [Vx],
+              [Vy]])
